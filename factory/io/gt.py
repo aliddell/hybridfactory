f4b8253a4708716e7276230bf8143db96a1e2e41
Alan Liddell <alan@vidriotech.com>
2018-05-10 09:07:20 -0400

    sort channels/times/labels by time order

--- factory/io/gt.py
+++ factory/io/gt.py
@@ -22,14 +22,18 @@ def save_gt_units(dirname, gt_channels, gt_times, gt_labels):
     assert counts == [t.size for t in gt_times]
     assert len(counts) == len(gt_labels)
 
+    # join all channels, times, labels into single arrays
     channels = np.hstack(gt_channels)
     times = np.hstack(gt_times)
     labels = np.repeat(gt_labels, counts)
 
+    # sort all by sample time, ascending
+    ordering = times.argsort()
+
     firings_true = np.zeros((3, np.sum(counts)), dtype=np.uint64)
 
-    firings_true[0, :] = channels
-    firings_true[1, :] = times
-    firings_true[2, :] = labels
+    firings_true[0, :] = channels[ordering]
+    firings_true[1, :] = times[ordering]
+    firings_true[2, :] = labels[ordering]
 
     np.save(op.join(dirname, "firings_true.npy"), firings_true)
