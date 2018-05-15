bea2a35b3a5e015904c75a903d9338f300766617
Alan Liddell <alan@vidriotech.com>
2018-05-15 13:57:22 -0400

    updates Neuropixels 3a probe file accordingly

--- factory/probes/npix3a.py
+++ factory/probes/npix3a.py
@@ -6,18 +6,18 @@ import numpy as np
 See https://github.com/cortex-lab/neuropixels/wiki/About_Neuropixels for details.
 """
 
-NCHANS = 384
+NCHANS = 385  # 384 channels + sync channel
 
 # the full channel map for the Neuropixels Phase 3A array
 channel_map = np.arange(NCHANS)
 
 # reference channels
-refchans = np.array([36, 75, 112, 151, 188, 227, 264, 303, 340, 379])
+refchans = np.array([36, 75, 112, 151, 188, 227, 264, 303, 340, 379, 384])
 connected = ~np.isin(channel_map, refchans)
 
 # physical location of each channel on the probe
-xcoords = np.tile([43, 11, 59, 27], NCHANS//4)  # 43 11 59 27 43 11 59 27 ...
-ycoords = 20 * np.repeat(1 + np.arange(NCHANS//2), 2)  # 20 20 40 40 ... 3820 3820 3840 3840
+xcoords = np.hstack((np.tile([43, 11, 59, 27], (NCHANS-1)//4), np.nan))  # 43 11 59 27 43 11 59 27 ...
+ycoords = 20 * np.hstack((np.repeat(1 + np.arange((NCHANS-1)//2), 2), np.nan))  # 20 20 40 40 ... 3820 3820 3840 3840
 channel_positions = np.hstack((xcoords[:, np.newaxis], ycoords[:, np.newaxis]))  # NCHANS x 2
 
 # default amount to shift by
