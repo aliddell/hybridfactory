47668b1bd9c162345498b97165e94e00d7c0c28d
Alan Liddell <alan@vidriotech.com>
2018-09-28 09:34:12 -0400

    adds PS script to run tests

---
+++ scripts/run_tests.ps1
@@ -0,0 +1,15 @@
+function Get-ProjectDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path (Split-Path $Invocation.MyCommand.Path)
+}
+
+$cwd=Get-Location
+$projectdir=Get-ProjectDirectory
+$anaconda="$env:HOMEPATH\Anaconda3\envs\hybridfactory\Scripts\"
+
+Set-Location "$projectdir"
+
+& "$anaconda\py.test.exe" --cov-conf test\.coveragerc --cov=hybridfactory test\
+
+Set-Location "$cwd"
